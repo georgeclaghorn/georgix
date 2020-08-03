234095a42d98e3a6cb4872df70700b23f9d657f9
George Claghorn <george.claghorn@gmail.com>
2020-08-03 10:02:26 -0400

    Style

--- src/arch/x86_64/util.rs
+++ src/arch/x86_64/util.rs
@@ -3,7 +3,7 @@ pub(super) fn rflags() -> u64 {
     let rflags: u64;
 
     unsafe {
-        asm!("pushf; pop {}", out(reg) rflags, options(nomem));
+        asm!("pushf", "pop {}", out(reg) rflags, options(nomem));
     }
 
     rflags
