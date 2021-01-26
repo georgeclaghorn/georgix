4bee7db54f045ab6b7a16ab743a3e30ae9c5c579
George Claghorn <george@hey.com>
2021-01-25 19:07:29 -0500

    Test align_up

--- src/util/alignment.rs
+++ src/util/alignment.rs
@@ -7,3 +7,18 @@ pub fn align_up(address: usize, alignment: usize) -> usize {
         (address + mask) & !mask
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn aligning_an_already_aligned_address_up() {
+        assert_eq!(0xFEE00000, align_up(0xFEE00000, 8))
+    }
+
+    #[test]
+    fn aligning_an_unaligned_address_up() {
+        assert_eq!(0xFEE00008, align_up(0xFEE00004, 8))
+    }
+}
