f56250d7f0df4c8575023ae562b644f53263a3f3
George Claghorn <george.claghorn@gmail.com>
2021-07-04 16:33:35 -0400

    Convert to default Intel syntax

--- src/arch/x86_64/boot/mod.rs
+++ src/arch/x86_64/boot/mod.rs
@@ -1,2 +1,2 @@
-global_asm!(include_str!("header.S"), options(att_syntax));
-global_asm!(include_str!("start.S"),  options(att_syntax));
+global_asm!(include_str!("header.S"));
+global_asm!(include_str!("start.S"));
